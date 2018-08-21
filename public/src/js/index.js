d4e2d896fc561f89d4fb580c0e62b3baa9959f13
Hugh Nguyen <hhnguyen255@gmail.com>
2018-08-21 11:28:09 +1000

    add mustache.js

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -4,49 +4,67 @@ const messages = document.getElementById('messages');
 const form = document.getElementById('message-form');
 const messageInput = document.getElementById('message');
 const locationBtn = document.getElementById('send-location');
+const messageTemplateText = document.getElementById('message-template-text').innerHTML;
+const messageTemplateLocation = document.getElementById('message-template-location').innerHTML;
 
-const clearInput = () => {
-    messageInput.value = '';
-};
-const getMessage = () => messageInput.value;
-const renderMessage = message => {
-    const formattedTime = moment(message.createdAt).format('h:mm a');
-    const markup = `<li>${message.from} (${formattedTime}): ${message.text}</li>`;
-    messages.insertAdjacentHTML('beforeend', markup);
+const clearMessageInput = () => messageInput.value = '';
+const getMessageInput = () => messageInput.value;
+
+const renderMessage = type => {
+    return function (message) {
+        const formattedTime = moment(message.createdAt).format('h:mm a');
+        
+        let template, data;
+        switch (type) {
+            case 'text':
+                template = messageTemplateText;
+                data = {
+                    from: message.from,
+                    createdAt: formattedTime,
+                    text: message.text
+                };
+                break;
+
+            case 'location':
+                template = messageTemplateLocation;
+                data = {
+                    from: message.from,
+                    createdAt: formattedTime,
+                    url: message.url
+                };
+                break;
+        }
+        
+        const markup = Mustache.render(template, data);
+        messages.insertAdjacentHTML('beforeend', markup);
+    };
 };
 
-const renderLocationMessage = message => {
-    const formattedTime = moment(message.createdAt).format('h:mm a');
-    const markup = `<li>${message.from} (${formattedTime}): <a href="${message.url}" target="_blank">My current location</a></li>`;
-    messages.insertAdjacentHTML('beforeend', markup);
-};
+const renderMessageText = renderMessage('text');
+const renderMessageLocation = renderMessage('location');
 
-socket.on('connect',() => console.log('Connected to server'));
+socket.on('connect', () => console.log('Connected to server'));
 
 socket.on('newMessage', message => {
-    if(message) {
-        renderMessage(message);
-    }
+    if (message) renderMessageText(message);
 });
 
 socket.on('newLocationMessage', message => {
-    if (message) {
-        renderLocationMessage(message);
-    }
+    if (message) renderMessageLocation(message);
 });
 
 form.addEventListener('submit', event => {
     event.preventDefault();
-    
-    const message = getMessage();
+
+    const message = getMessageInput();
     socket.emit('createMessage', {
         from: 'Hugh',
         text: message
-    }, function(data) {
-        console.log('Got it!', data);    
+    }, function (data) {
+        console.log('Got it!', data);
     });
-    
-    clearInput();
+
+    clearMessageInput();
 });
 
 locationBtn.addEventListener('click', event => {
